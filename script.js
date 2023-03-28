56f595d2a9008a58be194a10182b917260043a3b
KABUB001 <berangerkoffi98@gmail.com>
2023-03-28 06:55:35 +0200

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+let dragged
+const source = document.querySelectorAll("dropbox");
+console.log(source);
+
+const tab1=[1,2,3,10,20,1,10];
+const tab2 = [22,5,16,20,2,8,5];
+const tab = tab1.filter((item,index)=>tab1.indexOf(item)==index)
+console.log(tab)
